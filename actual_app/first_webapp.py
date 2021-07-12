56edd8f4038c5dadbc25cee0290b1a8d0360442c
icejjfrish <77500045+icejjfrish@users.noreply.github.com>
2021-07-12 08:34:13 -0400

    Update first_webapp.py

--- actual_app/first_webapp.py
+++ actual_app/first_webapp.py
@@ -4,6 +4,9 @@ Created on Mon Jun 28 15:34:17 2021
 
 @author: Jaclyn Frishcosy
 """
+import matplotlib.pyplot as plt
 
-print("This is a test for Heroku deploying."\n)
+print("This is a test for Heroku deploying.\n")
 print(1)
+
+plt.plot(2, 7)

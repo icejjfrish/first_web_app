58950bb033a373857ec0b4796ff220f19417825c
icejjfrish <77500045+icejjfrish@users.noreply.github.com>
2021-07-12 08:40:14 -0400

    Update first_webapp.py

--- actual_app/first_webapp.py
+++ actual_app/first_webapp.py
@@ -4,9 +4,17 @@ Created on Mon Jun 28 15:34:17 2021
 
 @author: Jaclyn Frishcosy
 """
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-print("This is a test for Heroku deploying.\n")
-print(1)
+# print("This is a test for Heroku deploying.\n")
+# print(1)
 
-plt.plot(2, 7)
+# plt.plot(2, 7)
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello this is the new version!"
